21e58f3689e6f26aa5b9a17404879d712aedb280
skullmasher <skullmasher@heartlessgaming.com>
2016-05-15 23:51:19 +0200

    Making the hogan templates work

--- app.js
+++ app.js
@@ -6,14 +6,21 @@ const path              = require('path')
 const postcssMiddleware = require('postcss-middleware')
 const autoprefixer      = require('autoprefixer')
 const chalk             = require('chalk')
-const app = express()
+const cons              = require('consolidate')
+const app               = express()
+
+
+// set .html as the default extension 
 
 const appDir = 'app'
 const buildDir = 'build'
 
-const routes = require( __dirname + '/' + appDir + '/routes/routes')(app)
+const routes = require(__dirname + '/' + appDir + '/routes/routes')(app)
 
 app
+  .engine('html', cons.hogan)
+  .set('view engine', 'html')
+  .set('views', __dirname + '/' + appDir + '/views')
   .use(sassMiddleware({
     /* Options */
     src: appDir,
@@ -37,9 +44,6 @@ app
   .use('/css', express.static('app/css'))
   .use('/js', express.static('app/js'))
   .use('/img', express.static('app/img'))
-  .get('/', function (req, res) {
-      res.send('lodr')
-  })
 
 let server = app.listen(3000, function () {
     console.log('Dev server started on ' + chalk.cyan('http://localhost:' + server.address().port))
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,12 +1,31 @@
 module.exports = function(app){
 
+  // app
+  //   .get('/', function(req, res){
+  //     res.send('Main page')
+  //   })
+  //   .get('/lodr', function (req, res) {
+  //     res.send('Hello there ! C\'est Skull')
+  //   })
+
+  let pageTitle = 'My awesome app'
+
+  let data = {
+    firstName: 'Skullmasher',
+    secondName: 'Heartless',
+    year: '2016'
+  }
+
   app
     .get('/', function(req, res){
-      res.send('Main page')
+      res.render('index', {
+        title: pageTitle
+      })
     })
-    .get('/lodr', function (req, res) {
-      res.send('Hello there ! C\'est Skull')
+    .get('/lodr', function(req, res){
+      res.render('lodr', {
+        title: 'Custom page title',
+        data: data
+      })
     })
-
-    //other routes..
 }
