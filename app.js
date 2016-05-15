1ee7a2a0092c7e0ca1e24288799515c91f8ec7e1
skullmasher <skullmasher@heartlessgaming.com>
2016-05-16 01:47:07 +0200

    Making CSS,JS & img folder static ressource within express

--- app.js
+++ app.js
@@ -2,18 +2,21 @@
 
 const express           = require('express')
 const sassMiddleware    = require('node-sass-middleware')
-const path              = require('path')
 const postcssMiddleware = require('postcss-middleware')
 const autoprefixer      = require('autoprefixer')
+const path      = require('path')
 const chalk             = require('chalk')
 const cons              = require('consolidate')
 const app               = express()
-
 
 // set .html as the default extension 
 
 const appDir = 'app'
 const buildDir = 'build'
+const sassDir = appDir + '/sass'
+const cssDir = appDir + '/css'
+const jsDir = appDir + '/js'
+const imgDir = appDir + '/sass'
 
 const routes = require(__dirname + '/' + appDir + '/routes/routes')(app)
 
@@ -23,27 +26,26 @@ app
   .set('views', __dirname + '/' + appDir + '/views')
   .use(sassMiddleware({
     /* Options */
-    src: appDir,
-    dest: buildDir,
+    src: path.join(__dirname, sassDir),
+    dest: path.join(__dirname, cssDir),
     debug: true,
     outputStyle: 'expended',
-    sourceMap: true,
-    prefix: '/prefix' // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+    sourceMap: true
   }))
-  .use(postcssMiddleware({
-    plugins: [
-      /* Plugins */
-      autoprefixer({
-        /* Options */
-      })
-    ],
-    src: function(req) {
-      return path.join(buildDir, req.url);
-    }
-  }))
-  .use('/css', express.static('app/css'))
-  .use('/js', express.static('app/js'))
-  .use('/img', express.static('app/img'))
+  // .use(postcssMiddleware({
+  //   plugins: [
+  //     /* Plugins */
+  //     autoprefixer({
+  //       /* Options */
+  //     })
+  //   ],
+  //   src: function(req) {
+  //     return __dirname + cssDir;
+  //   }
+  // }))
+  .use('/css', express.static(path.join(__dirname, cssDir)))
+  .use('/js', express.static(path.join(__dirname, jsDir)))
+  .use('/img', express.static(path.join(__dirname, imgDir)))
 
 let server = app.listen(3000, function () {
     console.log('Dev server started on ' + chalk.cyan('http://localhost:' + server.address().port))
