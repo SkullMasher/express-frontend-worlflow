8d431c6756d3725e4b0f30075aa2616ca71aee55
skullmasher <skullmasher@heartlessgaming.com>
2016-05-16 03:24:31 +0200

    Adding a default favicon

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 'use strict'
 
+const chalk             = require('chalk')
+const path              = require('path')
 const express           = require('express')
 const sassMiddleware    = require('node-sass-middleware')
 const postcssMiddleware = require('postcss-middleware')
 const autoprefixer      = require('autoprefixer')
-const path      = require('path')
-const chalk             = require('chalk')
-const cons              = require('consolidate')
+const consolidate       = require('consolidate')
 const app               = express()
-
-// set .html as the default extension 
+const favicon           = require('serve-favicon')
 
 const appDir = 'app'
 const buildDir = 'build'
 const sassDir = appDir + '/sass'
 const cssDir = appDir + '/css'
 const jsDir = appDir + '/js'
-const imgDir = appDir + '/sass'
+const imgDir = appDir + '/img'
 
-const routes = require(__dirname + '/' + appDir + '/routes/routes')(app)
+
+console.log(path.join(__dirname, imgDir, 'favicon.ico'))
 
 app
-  .engine('html', cons.hogan)
+  .engine('html', consolidate.hogan)
   .set('view engine', 'html')
   .set('views', __dirname + '/' + appDir + '/views')
+  .use(favicon(path.join(__dirname, imgDir, 'favicon.ico')))
   .use(sassMiddleware({
     /* Options */
     src: path.join(__dirname, sassDir),
@@ -47,7 +48,8 @@ app
   .use('/js', express.static(path.join(__dirname, jsDir)))
   .use('/img', express.static(path.join(__dirname, imgDir)))
 
+const routes = require(__dirname + '/' + appDir + '/routes/routes')(app)
+
 let server = app.listen(3000, function () {
     console.log('Dev server started on ' + chalk.cyan('http://localhost:' + server.address().port))
 })
-
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,13 +1,4 @@
 module.exports = function(app){
-
-  // app
-  //   .get('/', function(req, res){
-  //     res.send('Main page')
-  //   })
-  //   .get('/lodr', function (req, res) {
-  //     res.send('Hello there ! C\'est Skull')
-  //   })
-
   let pageTitle = 'My awesome app'
 
   let data = {
